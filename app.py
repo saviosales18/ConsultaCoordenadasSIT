bb17bd6bc09e719932cb099b9659c5f92d732f25
saviosales18 <saviosales18@gmail.com>
2023-09-01 09:33:42 -0300

    Version 1.3
    Adaptado para pesquisar o trecho encontrado no FXD na planilha sreCompleto.xlsx e com isso obter diversas outras informações como código SRE, Km inicial e final, Jurisdição, Coincidentes estaduais e federais, coordenadas inicias e finais do trecho, extensão da rodovia, Largura da faixa de domínio e o amparo legal.

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas
+import pandas as pd
 from time import sleep
 
 def get_coordinates(latitude, longitude, zona):
     # Abrindo o site no Chrome
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Remove do console a mentagem "DevTools listening on ws://127.0.0.1:58958/devtools/browser..."
     driver = webdriver.Chrome(options=chrome_options)
     driver.get('http://sider.derba.ba.gov.br/portalfxd/abrirConsultaCoordenada.do')
-    #driver.set_window_size(1024, 768)
-
+    
     # Selecionando Datum e zona
     dropdownDatum = driver.find_element(By.NAME, "cdSistcoordenada")
     opDatum = Select(dropdownDatum)
@@ -24,8 +24,10 @@ def get_coordinates(latitude, longitude, zona):
     elif zona == "24":
         opDatum.select_by_visible_text('SIRGAS_UTM_zona_24S')
 
-    # Colocando coordenadas latitude e longitude
-    campoLat = driver.find_element(By.NAME, "vlXcoord")
+    # Aguardar até que o campo de latitude esteja clicável
+    campoLat = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "vlXcoord"))
+    )
     campoLat.send_keys(latitude)
 
     campoLong = driver.find_element(By.NAME, "vlYcoord")
@@ -35,25 +37,51 @@ def get_coordinates(latitude, longitude, zona):
     btnConsultar = driver.find_element(By.XPATH, "//span[@class='spwButtonMain']")
     btnConsultar.click()
 
-    # Verifica se a mensagem de erro foi encontrada
-    mensagem_erro = driver.find_elements(By.ID, "spwMensagemId")
-    if mensagem_erro:
-        print("Nenhuma resposta encontrada:", mensagem_erro[0].text)
-    else:
-        # Extraindo informações de Rodovia, Trecho e Km
-        rodovia = driver.find_element(By.NAME, "nmRodovia")
-        trecho = driver.find_element(By.NAME, "nmTrecho")
-        km = driver.find_element(By.NAME, "vlKm")
+    # Extraindo informações de Rodovia, Trecho e Km
+    rodovia = driver.find_element(By.NAME, "nmRodovia")
+    trecho = driver.find_element(By.NAME, "nmTrecho")
+    km = driver.find_element(By.NAME, "vlKm")
 
-        print("Rodovia:     ", rodovia.get_attribute('value'))
-        print("Trecho:      ", trecho.get_attribute('value'))
-        print("Km:          ", km.get_attribute('value'))
+    print("Rodovia:          ", rodovia.get_attribute('value'))
+    print("Trecho:           ", trecho.get_attribute('value'))
+    print("Km:               ", km.get_attribute('value'))
+
+    # Nome para pesquisar
+    nome_pesquisa = trecho.get_attribute('value')
 
     driver.quit()
 
+    # Carregar a planilha
+    df = pd.read_excel("sreCompleto.xlsx")
+
+    # Filtrar as linhas que correspondem ao nome
+    filtro = df['TRECHO'] == nome_pesquisa
+    linha_encontrada = df.loc[filtro]
+
+    # Verificar se o nome foi encontro
+    if not linha_encontrada.empty:
+        cod = linha_encontrada['CÓDIGO'].values[0]
+        kmi = linha_encontrada['INICIAL'].values[0]
+        kmf = linha_encontrada['FINAL'].values[0]
+        kmt = linha_encontrada['EXTENÇÃO'].values[0]
+        mun = linha_encontrada['MUNICÍPIO'].values[0]
+        fxd = linha_encontrada['TOTAL FXD'].values[0]
+        amparo = linha_encontrada['AMPARO LEGAL'].values[0]
+        juris = linha_encontrada['JURISDIÇÃO'].values[0]
+        print(f"Código SRE:       {cod}")
+        print(f"Km Inicial:       {kmi}")
+        print(f"Km Final:         {kmf}")
+        print(f"Extenção Total:   {kmt}")
+        print(f"Município:        {mun}")
+        print(f"Jurisdição:       {juris}")
+        print(f"Largura da FXD:   {fxd}")
+        print(f"Amparo Legal:     {amparo}")
+    else:
+        print(f"Trecho {nome_pesquisa} não encontrado no SRE.")
+
 # Loop contínuo
 while True:
-    # Solicita entrada de latitude
+    # Solicita entrada de latitude e longitude
     nLatitude = input("Digite a latitude: ")
     nLongitude = input("Digite a longitude: ")
     nZona = input("Digite a zona (23 ou 24): ")
